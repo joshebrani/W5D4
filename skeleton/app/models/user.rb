f32924ea140658038ac30f6a03287a11bf4f24e8
joshebrani <joshuaebrani@gmail.com>
2021-01-28 14:02:28 -0500

    Started associations with enrollment and user

---
+++ skeleton/app/models/user.rb
@@ -0,0 +1,17 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class User < ApplicationRecord
+    has_many :courses,
+        primary_key: :id,
+        foreign_key: :course_id,
+        class_name: :Courses
+
+    
+end
